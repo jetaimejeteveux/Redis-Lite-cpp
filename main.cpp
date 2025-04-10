e6e04c9b2415692a09acbd8024abff3fb8d04fa2
jetaimejeteveux <akufirman22@gmail.com>
2025-04-10 20:43:44 +0700

    init project: feat: add main entry point

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "Redis lite c++ Server is starting..." << std::endl;
+    // TODO: Initialize store
+    // TODO: Initialize command handler
+    // TODO: Initialize persistence
+    // TODO: Initialize server
+    // TODO: Run server main loop
+
+    std::cout << "Redist lite c++ server is shutting down" << std::endl;
+    return 0;
+}
